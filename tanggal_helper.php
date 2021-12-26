6de34c1c51283695acedb835b3ed76a4adefdd7a
Gagas Sangga Pratama <49256437+haiigas@users.noreply.github.com>
2021-12-26 08:05:12 +0700

    Update tanggal_helper.php

--- tanggal_helper.php
+++ tanggal_helper.php
@@ -1,14 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-if (!function_exists('format_indo')) {
-  function format_indo($date){
+if (!function_exists('format_indo'))
+{
+  function format_indo($date)
+  {
     date_default_timezone_set('Asia/Jakarta');
-    // array hari dan bulan
+    /** array hari dan bulan */
     $Hari = array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
     $Bulan = array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
     
-    // pemisahan tahun, bulan, hari, dan waktu
+    /** pemisahan tahun, bulan, hari, dan waktu */
     $tahun = substr($date,0,4);
     $bulan = substr($date,5,2);
     $tgl = substr($date,8,2);
